22a9850ea881fcd495b201eb037a5cf7eb5ee9e8
ILYSHENKA <prilipal17@gmail.com>
2023-06-22 18:01:38 +0300

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "https://prilipal.github.io/test_exercise_vue_QD/" : "/",
+  publicPath: "test_exercise_vue_QD",
   transpileDependencies: true
 })
