9163e543e7c413c1c85b8a09c50f7e8e4474f0f5
ILYSHENKA <prilipal17@gmail.com>
2023-06-22 17:47:05 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "https://prilipal.github.io/test_exercise_vue_QD/" : "/",
   transpileDependencies: true
 })
